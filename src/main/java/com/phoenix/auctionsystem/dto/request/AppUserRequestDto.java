8ab4c10a862cafc2b97b11dd09b184ed7dc54b20
Tosin Akinnusi <akinnusitosinbenedict@gmail.com>
2022-04-03 21:08:54 +0100

    app user request dto

--- src/main/java/com/phoenix/auctionsystem/dto/request/AppUserRequestDto.java
+++ src/main/java/com/phoenix/auctionsystem/dto/request/AppUserRequestDto.java
@@ -1,7 +1,10 @@
 package com.phoenix.auctionsystem.dto.request;
 
+import lombok.Data;
+
 import javax.validation.constraints.*;
 
+@Data
 public class AppUserRequestDto {
     @NotNull(message = "Firstname must be provided")
     private String firstName;
@@ -12,13 +15,13 @@ public class AppUserRequestDto {
     @NotNull(message = "email address can't be null")
     @Email(regexp = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$",
             message = "provide a valid email address")
-    private String emailAddress;
+    private String email;
 
     @NotNull(message = "provide valid phone number")
     @Size(min = 11, max = 14, message = "phone number not valid")
     @Pattern(regexp = "^[\\+]?[(]?[0-9]{3}[)]?[-\\s\\.]?[0-9]{3}[-\\s\\.]?[0-9]{4,6}$",
             message = "provide valid phone number")
-    private String contactNumber;
+    private String phoneNumber;
 
     @NotNull(message = "provide contact address")
     private String address;
