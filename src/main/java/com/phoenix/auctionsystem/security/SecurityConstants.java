4575ba718fe107500ac13870223e5b3f8dae4da7
Tosin Akinnusi <akinnusitosinbenedict@gmail.com>
2022-05-29 22:30:14 +0100

    Security Constants

---
+++ src/main/java/com/phoenix/auctionsystem/security/SecurityConstants.java
@@ -0,0 +1,6 @@
+package com.phoenix.auctionsystem.security;
+
+public class SecurityConstants {
+    public static final String JWT_KEY = "MyAppSecret@!";
+    public static final String JWT_HEADER = "Authorization";
+}
